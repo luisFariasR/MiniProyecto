149c142892afb9d3e26375e1de1f97c8f057d645
Luis Farias <popshovit33@gmail.com>
2024-11-25 08:18:08 -0600

    mi primer commit

---
+++ scripts.js
@@ -0,0 +1,47 @@
+
+const modal = document.querySelector(`.modal-content`);
+const buttonDiss = document.querySelector(`.btn_dismiss`);
+const input = document.querySelector(`.input`);
+const btnSubs = document.querySelector(`.btn-subs`);
+const alert = document.querySelector(`.alert`)
+const textModal = document.querySelector(`.textModal`);
+
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+ 
+function showOf() {
+    const emailValue = input.value;
+  
+  if (validateEmail(emailValue)) {
+    modal.classList.remove(`d-none`);
+    const  mail = `
+    <p class="text-light">
+      A confirmation email has been sent to <br>
+      ${emailValue} Please open it and click <br> the button inside to
+      confirm your subscription.</p>`
+
+      textModal.innerHTML = mail;
+  } else {
+
+    alert.classList.remove(`d-none`);
+  }
+  
+  input.value = ""
+}
+function closeModal(){
+    modal.classList.add(`d-none`);
+    alert.classList.add(`d-none`);
+}
+
+buttonDiss.addEventListener(`click`, closeModal);
+btnSubs.addEventListener(`click`, showOf);
+
+
+
+
+
+
+
